d9f59d3cf7cf76908a55c4bb413e2a11b0c98124
n <83850208+510x@users.noreply.github.com>
2022-02-03 21:16:10 -0800

    Create new.lua
    I'll add more when I got time.

---
+++ new.lua
@@ -0,0 +1,52 @@
+any bugs / Questions @ saturn#1432
+note: If I have time and remember, I'll try to update this.
+Current Bugs: Unknown
+
+Prefixes:
+  [1] - Default Prefix = ","
+  [2] - Custom User Prefixes = Paid
+  [3] - Changeable Prefix
+        [3.1] - ,prefix set <new_prefix>
+        
+Misc:
+  1.View users avatar - avatar [av, pfp]
+    [1 - Syntax]: ,avatar <member>
+
+  2.View users banners - banner [ub]
+    [2 - Syntax]: banner <member>
+
+  3.Decode base64 - base64 (args)
+    [3 - Syntax]: base64 <decode>
+
+  4.Checks the bio of the member - bio <member>  // Some what Working
+    [4- Syntax]: bio <member>
+
+  5.check if the bot is alive - alive
+    [5 - Syntax]: N/A
+
+  6.heads or tails - coinflip (args)  [cf]
+    [6 - Syntax]: ,coinflip 
+  
+
+Embed parameters:
+  {author: hi} - Sets the author
+    use && for multiple parts
+    Example: {author: hi && https://url.com && https://url.com}
+  {title: hi} - Sets the title
+  {description: okay} - sets the description
+  {footer: hi} - Sets the footer
+    Example: {footer: hi && https://url.com}
+  {url:  https://url.com} - Sets the URL
+  {timestamp} - Sets the embed timestamp
+  {color: HEX} - Sets the embed color
+  {field: hi && okay && true} - Adds field
+    Example: {field: hi && gb && false}
+  {image: https://url.com} - Adds an image
+  {thumbnail: https://url.com} - Adds an thumbnail
+  {message: hi} - adds an optional message   
+
+  Examples:
+    {embed}$v{title: hi}$v{description: okay bye}$v{field: okay bye && bye bitch && false}$v{field: bye bye && bye bitch && false}$v{color: 36393f}$v{timestamp}
+
+
+ 
